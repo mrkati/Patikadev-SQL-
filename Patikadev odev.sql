8c1cfa525c7c2a4dfc44bc8b39f2ad1fea343d07
MURAT <murat_kati@hotmail.com>
2025-11-21 18:08:22 +0300

    Create Patikadev odev.sql

---
+++ Patikadev odev.sql
@@ -0,0 +1,12 @@
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT city, country FROM CITY
+INNER JOIN Country ON city.city_id = country.country_id;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental
+JOIN customer ON customer.customer_id = rental.rental_id;
